7aed5b2080a7e96bf014a5991f7ce176354cd02a
Bigov <bigov@yandex.ru>
2018-12-10 10:43:50 +0900

    rm glfw as git submodule

--- cmakes/main.cmake
+++ cmakes/main.cmake
@@ -10,16 +10,17 @@ include( "${trCMAKES}/params.cmake" )
 # подключение исходных файлов библиотек
 include( "${trCMAKES}/extlibs/glcore33.cmake" )
 include( "${trCMAKES}/extlibs/sqlite3.cmake" )
-include( "${trCMAKES}/extlibs/glfw.cmake" )
 
 # где искать заголовки
 include_directories( ${trINCLUDE_DIRS} )
 
 find_package( PkgConfig REQUIRED )
 pkg_check_modules( PNG REQUIRED libpng16 )
+pkg_check_modules( GLFW REQUIRED glfw3 )
+SET( trLIBS ${trLIBS} ${GLFW_LIBRARIES} ${PNG_LIBRARIES} )
 
 ## Список библиотек
-SET( trLIBS ${trLIBS} pthread stdc++fs glcore33 glfw sqlite3 ${PNG_LIBRARIES} )
+SET( trLIBS ${trLIBS} pthread stdc++fs glcore33 sqlite3 )
 
 add_executable( ${EXEC_NAME} ${trSRC} )
 target_link_libraries( ${EXEC_NAME} ${trLIBS} )
