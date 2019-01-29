c3ea08cf6827d7ef5ed1cacdc6608de17a74d6db
Igor Balezin <balezin@yandex.ru>
2019-01-29 21:27:54 +0900

    find

--- assets/mix/func_tests/arr_tst.cpp
+++ assets/mix/func_tests/arr_tst.cpp
@@ -3,25 +3,24 @@
 #include <vector>
 
 struct v_color {float r=1.0f, g=1.1f, b=1.2f, a=0.f;};  // цвет вершины - 4 числа
-using s_color = std::array <v_color, 4>;
+using s_color = std::array <v_color, 4>;                // сторона - 4 вершины по 4 числа
 
 int main(int, char**)
 {
-  s_color SideC {v_color{}, {}, {}, {}}; // цвет стороны - цвета 4-х вершин
+  s_color Side0 {v_color{}, {}, {}, {}}; // цвет стороны - цвета 4-х вершин
+  s_color s[6] = {Side0};                // шесть сторон
+  std::array <s_color*, 6> aSides  { &s[0], &s[0], &s[0], &s[0] };
 
-  std::vector<s_color> SidesClrs {s_color{}};
-  std::array <s_color*, 4> Clrs  { &(SidesClrs[0]), &(SidesClrs[0]) };
+  int side_id = 2;
+  int vertex_id = 3;
 
-  int Side = 0;
-  int Vert = 0;
-
-  s_color Ts = SidesClrs[0]; // цвета стороны
-  v_color Tv = Ts[0];
-  float c = Tv.r;
+  s_color* mySide = aSides[side_id]; // цвета стороны
+  s_color Vertex_array = mySide[3];
+  float c = Vertex_array[vertex_id].r;
 
   std::cout
-     << Clrs[1][Side][Vert].b
-     << "\nok\n";
+     //<< aSides[1][Side][Vert].b
+     << "\n ok \n";
 
   return EXIT_SUCCESS;
 }
