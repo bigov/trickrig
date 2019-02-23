afd889afa0ced034d4014730ab1f5889d11c8b5c
bigov <bigov@yandex.com>
2019-02-23 22:53:56 +0900

    add core key

--- assets/sel_frag.glsl
+++ assets/sel_frag.glsl
@@ -1,4 +1,4 @@
-#version 330
+#version 330 core
 
 out uvec3 FragColor;
 //uniform uint DrawIndex;
--- assets/sel_vert.glsl
+++ assets/sel_vert.glsl
@@ -1,4 +1,4 @@
-#version 330
+#version 330 core
 
 in vec4 position;
 //in vec4 color;     // цвет точки
