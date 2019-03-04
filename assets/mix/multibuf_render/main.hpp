41a1e3bdbbde99e88c300ee5187cb4b767ef1891
Bigov <bigov@yandex.ru>
2019-03-04 19:14:30 +0900

    add test project MBR

---
+++ assets/mix/multibuf_render/main.hpp
@@ -0,0 +1,40 @@
+/* filename main.hpp
+ *
+ */
+#ifndef main_hpp
+#define main_hpp
+
+#include <memory>
+#include <iostream>
+#include <chrono>
+//#include <string>
+#include <cstring>
+#include <vector>
+#include "glad.h"
+#include <sys/stat.h>
+#include <fstream>
+
+//#include <any>
+//#include <array>
+//#include <cassert>
+//#include <cstdint>
+//#include <chrono>
+//#include <forward_list>
+//#include <functional>
+//#include <iomanip>
+//#include <iostream>
+//#include <list>
+//#include <locale>
+//#include <map>
+//#include <random>
+//#include <sstream>
+//#include <string>
+//#include <valarray>
+//#include <vector>
+//#include <unordered_map>
+//#include <utility>
+//#include <filesystem>
+
+#define ERR throw std::runtime_error
+
+#endif
