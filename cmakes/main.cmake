cce23699112e2edcbfc402a1daad78bc1a122dbb
bigov <bigov@yandex.ru>
2019-12-14 14:36:03 +0900

    fix cmake

--- cmakes/main.cmake
+++ cmakes/main.cmake
@@ -18,9 +18,12 @@ pkg_check_modules( PNG REQUIRED libpng16 )
 pkg_check_modules( GLFW REQUIRED glfw3 )
 pkg_check_modules( SQLITE REQUIRED sqlite3 )
 
+pkg_check_modules( GLM REQUIRED glm )
+include_directories( ${GLM_INCLUDE_DIRS} )
+
 ## Список библиотек
 SET( trLIBS ${trLIBS} ${GLFW_LIBRARIES} ${PNG_LIBRARIES} ${SQLITE_LIBRARIES}
-  pthread stdc++fs glad )
+   pthread stdc++fs glad )
 
 add_executable( ${EXEC_NAME} ${trSRC} )
 target_link_libraries( ${EXEC_NAME} ${trLIBS} )
--- include/main.hpp
+++ include/main.hpp
@@ -37,14 +37,14 @@
 
 #include "glad.h"
 #define GLM_FORCE_RADIANS
-#include "glm/glm.hpp"
-#include "glm/gtc/constants.hpp"
-#include "glm/gtc/integer.hpp"
-#include "glm/gtc/matrix_transform.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/gtc/integer.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #define GLM_ENABLE_EXPERIMENTAL
-#include "glm/gtx/rotate_vector.hpp"
-#include "glm/gtx/transform.hpp"
-#include "glm/gtc/type_ptr.hpp"
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "i_win.hpp"
 
